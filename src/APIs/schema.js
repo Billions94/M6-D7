96ab4c09143258fcb8b414b2350f05b7c598f787
Ejiroghene Alexander Egbedi <86908505+Billions94@users.noreply.github.com>
2021-11-09 17:55:13 +0100

    complete

--- src/APIs/schema.js
+++ src/APIs/schema.js
@@ -1,25 +1,33 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const { Schema, model } = mongoose
+const { Schema, model } = mongoose;
 
 const blogPostSchema = new Schema(
-    {
-	    category: { type: String, required: true},
-	    title: { type: String, required: true},
-	    cover: { type: String, required: true},
-	    readTime: {
-	      value: { type: Number },
-	      unit: { type: Number },
-	    },
-	    author: {
-	      name: { type: String, required: true},
-	      avatar: { type: String, required: true},
-	    },
-	    content: { type: String, required: true},
+  {
+    category: { type: String, required: true },
+    title: { type: String, required: true },
+    cover: { type: String, required: true },
+    readTime: {
+      type: Object,
+      required: true,
+      nested: {
+        value: { type: Number },
+        unit: { type: Number },
+      },
     },
-    {
-        timestamps: true
-    }
-    )
+    author: {
+      type: Object,
+      required: true,
+      nested: {
+        name: { type: String, required: true },
+        avatar: { type: String, required: true },
+      },
+    },
+    content: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-    export default model('Blog', blogPostSchema)
+export default model("Blog", blogPostSchema);
